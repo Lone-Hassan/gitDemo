834ca90d5aa8e14799ad36f6371c8294ba5e113e
hassan <lone.hassan@gmail.com>
2024-01-25 18:53:29 +0500

    created new class from gitstuff

---
+++ src/main/java/PojoClasses/gitstuff.java
@@ -0,0 +1,11 @@
+package PojoClasses;
+
+public class gitstuff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("must be downloaded");
+
+	}
+
+}

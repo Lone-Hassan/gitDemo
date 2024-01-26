3369f286418f1109fb91ee56694066222cae2a9e
hassan <lone.hassan@gmail.com>
2024-01-26 15:10:41 +0500

    architect in development branch

--- src/main/java/PojoClasses/gitstuff.java
+++ src/main/java/PojoClasses/gitstuff.java
@@ -5,6 +5,8 @@ public class gitstuff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("must be downloaded");
+		System.out.println("changed to develop branch");
+		
 
 	}
 

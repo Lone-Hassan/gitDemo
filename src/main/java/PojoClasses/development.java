1871de01fc28c6938b6fb770d10022d7e37043d8
hassan <lone.hassan@gmail.com>
2024-01-26 15:22:04 +0500

    git demo created new development .java file

---
+++ src/main/java/PojoClasses/development.java
@@ -0,0 +1,10 @@
+package PojoClasses;
+
+public class development {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("only for develop branch");
+	}
+
+}
